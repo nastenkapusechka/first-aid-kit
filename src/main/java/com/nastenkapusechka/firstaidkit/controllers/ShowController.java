c4f2c78b388ac5136b375bcd24a263be509d810b
nastenkapuse4ka <nbakhshieva333@gmail.com>
2020-11-09 17:15:56 +0300

    add jpa support

--- src/main/java/com/nastenkapusechka/firstaidkit/controllers/ShowController.java
+++ src/main/java/com/nastenkapusechka/firstaidkit/controllers/ShowController.java
@@ -1,11 +1,21 @@
 package com.nastenkapusechka.firstaidkit.controllers;
 
+import com.google.common.base.Preconditions;
 import com.nastenkapusechka.firstaidkit.models.Pill;
 import com.nastenkapusechka.firstaidkit.repo.PillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Optional;
 
 @Controller
 public class ShowController {
@@ -18,5 +28,42 @@ public class ShowController {
         Iterable<Pill> pills = repository.findAll();
         model.addAttribute("pills", pills);
         return "show";
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestParam String name, @RequestParam String description,
+                      @RequestParam String form, @RequestParam String date, Model model) {
+
+        Date goodDate;
+
+        try{
+            Preconditions.checkArgument(!date.equals(""));
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            goodDate = format.parse(date);
+        } catch (IllegalArgumentException | ParseException e) {
+            goodDate = new Date();
+        }
+
+        Pill pill = new Pill(name, description, form, goodDate);
+        repository.save(pill);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/add/form")
+    public String addForm(Model model) {
+        return "add";
+    }
+
+    @GetMapping("/show/{id}")
+    public String details(@PathVariable(value = "id") long id, Model model) {
+        if (!repository.existsById(id)) {
+            return "redirect:/";
+        }
+        Optional<Pill> pill = repository.findById(id);
+        ArrayList<Pill> pills = new ArrayList<>();
+        pill.ifPresent(pills :: add);
+        model.addAttribute("pills", pills);
+        return "details";
     }
 }
--- src/main/java/com/nastenkapusechka/firstaidkit/models/Pill.java
+++ src/main/java/com/nastenkapusechka/firstaidkit/models/Pill.java
@@ -1,15 +1,18 @@
 package com.nastenkapusechka.firstaidkit.models;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Comparator;
 import java.util.Date;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Pill {
 
     @Id
@@ -20,4 +23,11 @@ public class Pill {
     private String description;
     private String form;
     private Date date;
+
+    public Pill(String name, String description, String form, Date date) {
+        this.name = name;
+        this.description = description;
+        this.form = form;
+        this.date = date;
+    }
 }
